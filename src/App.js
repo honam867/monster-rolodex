72edcf05ca2382ce3e62c4824c70560dccccafb6
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2020-09-21 11:42:47 +0700

    first react app from scratch again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CartList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch("https://portaltlt.azurewebsites.net/api/customer")
+      .then((respone) => respone.json())
+      .then((users) =>
+        this.setState({
+          monsters: users,
+        })
+      );
+  }
+
+  handlechange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handlechange={(e) => {
+            this.handlechange(e);
+          }}
+        />
+        <CartList monster={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
